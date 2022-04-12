8f6966b17f1ec7cfcec79e4ecc2f5aba6454c143
DangNguyen146 <19521317@gm.uit.edu.vn>
2022-04-13 03:04:55 +0700

    lab1

---
+++ app/src/main/java/com/example/lab03_2_1/DatabaseHelper.java
@@ -0,0 +1,33 @@
+package com.example.lab03_2_1;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_CREATE = "create table users (_id integer primary key autoincrement, "
+            + "name text not null);";
+
+
+    public DatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // TODO Auto-generated method stub
+        db.execSQL(DATABASE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+    }
+}
